20f6faec5e541696593543c9415329f2d80eaf54
castlehoo <ksungho9991@gmail.com>
2025-05-17 01:14:36 +0900

    UPDATE:New Coding_TEST file

---
+++ May/16_May/pd_leetcode_619.py
@@ -0,0 +1,85 @@
+# 619. Biggest Single Number
+# Solved
+# Easy
+# Topics
+# Companies
+# SQL Schema
+# Pandas Schema
+# Table: MyNumbers
+
+# +-------------+------+
+# | Column Name | Type |
+# +-------------+------+
+# | num         | int  |
+# +-------------+------+
+# This table may contain duplicates (In other words, there is no primary key for this table in SQL).
+# Each row of this table contains an integer.
+ 
+
+# A single number is a number that appeared only once in the MyNumbers table.
+
+# Find the largest single number. If there is no single number, report null.
+
+# The result format is in the following example.
+
+ 
+
+# Example 1:
+
+# Input: 
+# MyNumbers table:
+# +-----+
+# | num |
+# +-----+
+# | 8   |
+# | 8   |
+# | 3   |
+# | 3   |
+# | 1   |
+# | 4   |
+# | 5   |
+# | 6   |
+# +-----+
+# Output: 
+# +-----+
+# | num |
+# +-----+
+# | 6   |
+# +-----+
+# Explanation: The single numbers are 1, 4, 5, and 6.
+# Since 6 is the largest single number, we return it.
+# Example 2:
+
+# Input: 
+# MyNumbers table:
+# +-----+
+# | num |
+# +-----+
+# | 8   |
+# | 8   |
+# | 7   |
+# | 7   |
+# | 3   |
+# | 3   |
+# | 3   |
+# +-----+
+# Output: 
+# +------+
+# | num  |
+# +------+
+# | null |
+# +------+
+# Explanation: There are no single numbers in the input table so we return null.
+
+import pandas as pd
+import numpy as np
+
+def biggest_single_number(my_numbers: pd.DataFrame) -> pd.DataFrame:
+    counts = my_numbers['num'].value_counts()
+
+    single = counts[counts == 1].index
+
+    if not single.empty:
+        return pd.DataFrame({'num' : [max(single)]})
+    else:
+        return pd.DataFrame({"num" : [np.nan]})
