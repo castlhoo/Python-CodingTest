790ea24b7207e95753c575cd10533b2286d317f5
castlehoo <ksungho9991@gmail.com>
2025-05-19 01:13:07 +0900

    UPDATE:New Coding_TEST file

---
+++ May/18_May/Ad_Example_2_3.py
@@ -0,0 +1,10 @@
+n,k = map(int, input().split())
+a = list(int, input().split())
+res = set()
+
+for i in range(n):
+    for j in range(i+1, n):
+        for m in range(j+1, n):
+            res.add(a[i]+a[j]+a[m])
+res = list(res)
+print(res[k-1])
