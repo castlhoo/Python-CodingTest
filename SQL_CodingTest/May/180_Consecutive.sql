024abdf1ca0f6bead1820f19b5cca595ef91d7af
castlehoo <ksungho9991@gmail.com>
2026-05-06 02:33:34 -0700

    UPDATE_20260506

---
+++ SQL_CodingTest/May/180_Consecutive.sql
@@ -0,0 +1,65 @@
+-- 180. Consecutive Numbers
+-- Solved
+-- Medium
+-- Topics
+-- premium lock icon
+-- Companies
+-- SQL Schema
+-- Pandas Schema
+-- Table: Logs
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | num         | varchar |
+-- +-------------+---------+
+-- In SQL, id is the primary key for this table.
+-- id is an autoincrement column starting from 1.
+ 
+
+-- Find all numbers that appear at least three times consecutively.
+
+-- Return the result table in any order.
+
+-- The result format is in the following example.
+
+ 
+
+-- Example 1:
+
+-- Input: 
+-- Logs table:
+-- +----+-----+
+-- | id | num |
+-- +----+-----+
+-- | 1  | 1   |
+-- | 2  | 1   |
+-- | 3  | 1   |
+-- | 4  | 2   |
+-- | 5  | 1   |
+-- | 6  | 2   |
+-- | 7  | 2   |
+-- +----+-----+
+-- Output: 
+-- +-----------------+
+-- | ConsecutiveNums |
+-- +-----------------+
+-- | 1               |
+-- +-----------------+
+-- Explanation: 1 is the only number that appears consecutively for at least three times.
+
+# Write your MySQL query statement below
+WITH CTE AS(
+    SELECT 
+        num,
+        LEAD(num,1) OVER(ORDER BY id) as Next_1,
+        LEAD(num,2) OVER(ORDER BY id) as Next_2
+    FROM 
+        Logs
+)
+SELECT
+    DISTINCT num AS ConsecutiveNums
+FROM CTE
+WHERE
+    num = Next_1 AND num = Next_2
